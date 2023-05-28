ce757f9f58de5bbe2f156b260d74ff961f75097b
Miniyahil Kebede <miniye6453@gmail.com>
2023-05-28 03:17:56 +0300

    full name updated

--- src/data/general_data.ts
+++ src/data/general_data.ts
@@ -1,6 +1,7 @@
 export const personal_info={
-    fullName:'Miniyahil Kebede',
-    subtitle:' Experienced Software Engineer from Ethiopia with 4+ years of experience'
+    fullName:'Miniyahil Kebede Loha',
+    subtitle:' Experienced Software Engineer from Ethiopia with 4+ years of experience',
+    email:'miniye6453@gmail.com'
 }
 
 export const links={
