76f9f14212669bd3bc45e5f6b5be22dd7ec18a56
Miniyahil Kebede <miniye6453@gmail.com>
2023-06-06 01:39:04 +0300

    project media added

--- src/data/project_data.ts
+++ src/data/project_data.ts
@@ -5,11 +5,11 @@ export const project_data: Array<ProjectProps> = [
     name: "One card system",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia totam, autem tenetur saepe eum quaerat, repudiandae alias esse nulla animi ullam vero ipsam molestias consequuntur. Molestiae nemo vitae natus! Quis?",
-    link: "www.sis.ju.edu.et",
+    link: "https://www.sis.ju.edu.et",
     company:'Jimma University',
     technologies:["React","Laravel","Mysql"],
     image_url:
-      "https://cdn-icons-png.flaticon.com/512/5433/5433756.png?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
+      "/media/sis.png",
   },
   {
     name: "Student Admission and Placement System",
@@ -18,8 +18,7 @@ export const project_data: Array<ProjectProps> = [
     link: "https://saps.ethernet.edu.et/",
     company:'Ministry of education',
     technologies:["React","NextJs","Spring Boot","Python"],
-    image_url:
-      "https://cdn-icons-png.flaticon.com/512/5433/5433756.png?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
+    image_url: "/media/saps2.png",
   },
     {
     name: "National Agridata hub",
@@ -29,7 +28,7 @@ export const project_data: Array<ProjectProps> = [
     company:'Ministry of Agriculture',
     technologies:["NestJs","NextJs","ArrangoDB","Jenkins"],
     image_url:
-      "https://cdn-icons-png.flaticon.com/512/5433/5433756.png?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
+      "/media/ag-datahub-home.png",
   },
    {
     name: "Budget Tool System",
@@ -80,5 +79,24 @@ export const project_data: Array<ProjectProps> = [
     company:'Wolaita people',
     image_url:
       "https://cdn-icons-png.flaticon.com/512/5433/5433756.png?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
+  },
+   {
+    name: "Telemedicine",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia totam, autem tenetur saepe eum quaerat, repudiandae alias esse nulla animi ullam vero ipsam molestias consequuntur. Molestiae nemo vitae natus! Quis?",
+    link: "https://telement.ju.edu.et/",
+    technologies:["Android","SMS","Java","Firebase"],
+    company:'Oromia region Health Beurue',
+    image_url: "/media/telemedicine.png",
+  },
+  {
+    name: "Ethiopian Universities App",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia totam, autem tenetur saepe eum quaerat, repudiandae alias esse nulla animi ullam vero ipsam molestias consequuntur. Molestiae nemo vitae natus! Quis?",
+    link: "https://github.com/hmhard/ethiopian-universities-info",
+    technologies:["Flutter","Dart","Google Map","Appwrite"],
+    company:'Open source',
+    image_url:
+      "/media/ethio.jpg",
   },
 ];
